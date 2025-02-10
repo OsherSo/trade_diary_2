717cf0b2023d757c05dd6fe5aebf92f8e7aa0b97
Osher <solimani.osher@gmail.com>
2025-02-10 05:02:33 +0200

    Change link to

--- client/src/components/error/UnexpectedError.jsx
+++ client/src/components/error/UnexpectedError.jsx
@@ -15,7 +15,7 @@ const UnexpectedError = ({ message }) => {
           </p>
         </div>
         <Link
-          to="/"
+          to="/dashboard"
           className="mt-8 inline-flex items-center px-6 py-3 text-base font-medium rounded-lg text-white bg-blue-600 hover:bg-blue-700 transition-colors duration-200"
         >
           <HomeIcon className="w-5 h-5 mr-2" />
